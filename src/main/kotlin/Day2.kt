d71c0021d0914081e7f88d3390c2d36bf951c549
Eduard Reger <eduard.reger@gmail.com>
2022-12-05 06:29:53 +0100

    Day 2

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,43 @@
+import utils.Input
+
+fun main() {
+    println("Day 2")
+    val lines = Input.readAsLines("input/2")
+
+    //Rock A X
+    //Paper B Y
+    //Scissors C Z
+    val combinations = mapOf(
+        "A X" to 4,
+        "A Y" to 8,
+        "A Z" to 3,
+        "B X" to 1,
+        "B Y" to 5,
+        "B Z" to 9,
+        "C X" to 7,
+        "C Y" to 2,
+        "C Z" to 6,
+    )
+
+    val scoreA = lines.sumOf { combinations[it]!! }
+
+    println("Strategy A Score: $scoreA")
+
+    //X lose
+    //Y draw
+    //Z win
+    val replacements = mapOf(
+        "A X" to "A Z",
+        "A Y" to "A X",
+        "A Z" to "A Y",
+        "B X" to "B X",
+        "B Y" to "B Y",
+        "B Z" to "B Z",
+        "C X" to "C Y",
+        "C Y" to "C Z",
+        "C Z" to "C X",
+    )
+    val scoreB = lines.map { replacements[it] }.sumOf { combinations[it]!! }
+    println("Strategy B Score: $scoreB")
+}
+
